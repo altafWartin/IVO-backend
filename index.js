963ce5bf7b5ee3cd997186f65c24f48127941424
altafWartin <157889037+altafWartin@users.noreply.github.com>
2024-05-10 17:02:47 +0530

    use socket.js

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ require("dotenv").config();
 
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
-
 const app = express();
 const server = http.createServer(app);
-// const io = socketIo(server);
-
 const cors = require("cors");
 const mongoose = require("mongoose");
 const sls = require("serverless-http");
 const { initializeApp, applicationDefault } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
-
 const socketIO = require("socket.io");
 const bodyParser = require("body-parser"); // Import body-parser middleware
+const path = require("path");
+const { createServer } = require("http");
 
+const { getIO, initIO } = require("./socket");
+
+app.use("/", express.static(path.join(__dirname, "static")));
+
+const httpServer = createServer(app);
 
 app.use(bodyParser.json());
 
@@ -54,25 +56,23 @@ app.use("/api", chatRoutes);
 
 // server.js
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-
-const port =  7000;
-
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
+const port = 7000;
 
 app.get("/", async (req, res, next) => {
-  res
-    .status(200)
-    .send(`This is IVO project working at port ${port} !!!!!!!!!`);
+  res.status(200).send(`This is IVO project working at port ${port} !!!!!!!!!`);
 });
 
 console.log("home page");
 
+initIO(httpServer);
 
+httpServer.listen(port);
+console.log(`http://localhost:${port}`);
+
+getIO();
